6f85243fe31c6a80c02cfadde57b698a240727e0
user12043 <user12043@hotmail.com>
2019-12-21 10:27:51 +0300

    look and feel setting fix

--- src/main/java/ogr/user12043/jpomotimer/Main.java
+++ src/main/java/ogr/user12043/jpomotimer/Main.java
@@ -13,20 +13,23 @@ public class Main {
     public static void main(String[] args) {
         Utils.loadSettings();
 
-        /* Set the Nimbus look and feel */
+        /* Set the look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+        /* If Windows or CDE/Motif is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Windows".equals(info.getName()) || "CDE/Motif".equals(info.getName())) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                } else if ("CDE/Motif".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
         } catch (Exception ex) {
-            System.err.println("Can not set Nimbus look and feel");
+            System.err.println("Can not set look and feel");
         }
         //</editor-fold>
         SwingUtilities.invokeLater(() -> {
